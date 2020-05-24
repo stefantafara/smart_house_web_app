511463e92678ec7098c7adce2aaaf01ae1a09d36
Stefan Tafara <tafara.stefan@gmail.com>
2020-05-24 18:31:46 +0300

    + saving to databases form data

--- coursera_house/core/form.py
+++ coursera_house/core/form.py
@@ -4,7 +4,13 @@ from .models import Setting
 
 class ControllerForm(forms.Form):
     # import pdb; pdb.set_trace()
+    '''
     bedroom_target_temperature = forms.IntegerField(initial=Setting.objects.filter(controller_name='bedroom_target_temperature')[0].value, min_value=16, max_value=50)
-    hot_water_target_temperature = forms.IntegerField(initial=Setting.objects.filter(controller_name='hot_water_target_temperature')[0].value, min_value=16, max_value=50)
-    bedroom_light = forms.BooleanField(initial=Setting.objects.filter(controller_name='bedroom_light')[0].value)
-    bathroom_light = forms.BooleanField(initial=Setting.objects.filter(controller_name='bathroom_light')[0].value)
+    hot_water_target_temperature = forms.IntegerField(initial=Setting.objects.filter(controller_name='hot_water_target_temperature')[0].value, min_value=24, max_value=90)
+    bedroom_light = forms.BooleanField(initial=Setting.objects.filter(controller_name='bedroom_light')[0].value, required=False)
+    bathroom_light = forms.BooleanField(initial=Setting.objects.filter(controller_name='bathroom_light')[0].value, required=False)
+'''
+    bedroom_target_temperature = forms.IntegerField(min_value=16, max_value=50)
+    hot_water_target_temperature = forms.IntegerField(min_value=24, max_value=90)
+    bedroom_light = forms.BooleanField(required=False)
+    bathroom_light = forms.BooleanField(required=False)
--- coursera_house/core/views.py
+++ coursera_house/core/views.py
@@ -1,5 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import FormView
+from django.shortcuts import render
 
 from .models import Setting
 from .form import ControllerForm
@@ -14,14 +15,53 @@ class ControllerView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super(ControllerView, self).get_context_data()
-        settings = Setting.objects.all()
-        print(f'settings: {settings}')
         context['data'] = controller_polling()
         print(context)
         return context
 
     def get_initial(self):
-        return {}
+        # import pdb; pdb.set_trace()
+        return {'bedroom_target_temperature': Setting.objects.filter(controller_name='bedroom_target_temperature')[0].value,
+                'hot_water_target_temperature': Setting.objects.filter(controller_name='hot_water_target_temperature')[0].value,
+                'bedroom_light': True if Setting.objects.filter(controller_name='bedroom_light')[0].value == 1 else False,
+                'bathroom_light': True if Setting.objects.filter(controller_name='bathroom_light')[0].value == 1 else False
+                }
 
     def form_valid(self, form):
+        # import pdb; pdb.set_trace()
+        print('trying to save to database...')
+
+        # getting objects from databases
+        bedroom_target_temperature = Setting.objects.filter(controller_name='bedroom_target_temperature')[0]
+        hot_water_target_temperature = Setting.objects.filter(controller_name='hot_water_target_temperature')[0]
+        bedroom_light = Setting.objects.filter(controller_name='bedroom_light')[0]
+        bathroom_light = Setting.objects.filter(controller_name='bathroom_light')[0]
+
+        # saving to database
+        bedroom_target_temperature.value = form.cleaned_data['bedroom_target_temperature']
+        bedroom_target_temperature.save()
+        hot_water_target_temperature.value = form.cleaned_data['hot_water_target_temperature']
+        hot_water_target_temperature.save()
+        bedroom_light.value = form.cleaned_data['bedroom_light']
+        bedroom_light.save()
+        bathroom_light.value = form.cleaned_data['bathroom_light']
+        bathroom_light.save()
+        print('Saved to database.')
         return super(ControllerView, self).form_valid(form)
+'''
+    def post(self, request):
+        # import pdb; pdb.set_trace()
+        print('trying to save to database...')
+        # getting objects from databases
+        bedroom_target_temperature = Setting.objects.filter(controller_name='bedroom_target_temperature')[0]
+        hot_water_target_temperature = Setting.objects.filter(controller_name='hot_water_target_temperature')[0]
+        bedroom_light = Setting.objects.filter(controller_name='bedroom_light')[0]
+        bathroom_light = Setting.objects.filter(controller_name='bathroom_light')[0]
+        # saving to database
+        bedroom_target_temperature.value = int(request.POST['bedroom_target_temperature'])
+        hot_water_target_temperature.value = int(request.POST['hot_water_target_temperature'])
+        bedroom_light.value = int(request.POST['bedroom_light'])
+        bathroom_light.value = int(request.POST['bathroom_light'])
+        int(request.POST['bedroom_target_temperature'])
+        # return render(request, template_name)
+        return self.form_valid()'''
